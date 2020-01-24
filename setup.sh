75e4f4c993a8af855eef0ce1f922321bd52913a3
Ervin Szilagyi <eszilagyi@riasolutionsgroup.com>
2020-01-24 12:47:45 +0200

    Added REST client.

--- setup.sh
+++ setup.sh
@@ -48,4 +48,7 @@ snap install slack --classic
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install ./google-chrome-stable_current_amd64.deb -y
 
+# install REST client
+snap install insomnia
+
 echo "--------INSTALLATION FINISHED--------"
